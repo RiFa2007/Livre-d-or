507eca034ac17cc05112e308414e67766db63030
irfane ahmed <irfaneahmed1009@gmail.com>
2026-06-03 16:20:09 +0400

    Add importmap:install to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Installer les assets JS
+php bin/console importmap:install
+
 # Lancer les migrations
 php bin/console doctrine:migrations:migrate --no-interaction --env=prod
 
