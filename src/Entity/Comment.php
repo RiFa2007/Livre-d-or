a067eddf853ad6a5547198b181acd16f44c4abf1
irfane ahmed <irfaneahmed1009@gmail.com>
2026-06-04 14:17:02 +0400

    feat : ajout de tests

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -39,6 +39,9 @@ class Comment
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $photoFilename = null;
 
+    #[ORM\Column(length: 255,options:['default'=>'submitted'])]
+    private ?string $state = 'submitted';
+
     public function getId(): ?int
     {
         return $this->id;
@@ -121,4 +124,17 @@ class Comment
 
         return $this;
     }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
 }
