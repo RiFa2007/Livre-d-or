8d7e5553e494d59ec492f1abc016c0541715fa85
irfane ahmed <irfaneahmed1009@gmail.com>
2026-06-05 17:02:50 +0400

    Feat : Add an spa

---
+++ spa/src/app.js
@@ -0,0 +1,50 @@
+/** @jsx h */
+/** @jsxFrag Fragment */
+import '../assets/styles/app.scss';
+import { h, render } from 'preact';
+import { Router, Link } from 'preact-router';
+import {useState, useEffect} from 'preact/hooks';
+import {findConferences} from './api/api';
+import Home from './pages/home';
+import Conference from './pages/conference';
+
+function App() {
+
+    const [conferences,setConferences] = useState([]);
+
+    useEffect(()=>{
+        findConferences().then((conferences)=>setConferences(conferences))
+    },[]);
+
+
+
+    return (
+        <div>
+                <header className="header">
+                    <nav className="navbar navbar-light bg-light">
+                        <div className="container">
+                            <Link className="navbar-brand mr-4 pr-2" href="/">
+                                &#128217; Guestbook
+                            </Link>
+                        </div>
+                    </nav>
+
+                    <nav className="bg-light border-bottom text-center">
+                        {conferences.map((conference)=>(
+                            <link className='nav-conference' href={'/conference/'+conference.slug}>
+                                {conference.city} {conference.year}
+                            </link>
+                        ))}
+                    </nav>
+                </header>
+
+                <Router>
+                <Home path="/" conferences={conferences} />
+                <Conference path="/conference/:slug" conferences={conferences} />
+                </Router>
+
+        </div>
+    );
+}
+
+render(<App />, document.getElementById('app'));
